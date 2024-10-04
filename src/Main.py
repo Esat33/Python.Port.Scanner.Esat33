76eec46bccb6f7290f7e880826f8f013edd8db86
Esat33 <mehmetesatakdogan47@gmail.com>
2024-10-04 22:37:47 +0300

    port sacanner exe

--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,25 @@
 import socket
 import pyfiglet
+import  os
+import  platform
+
 
 
 def main():
+ count ="1"
  ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
  print(ascii_banner)
 
  print("Enter the IP address you want to scan (IPv4 only): ")
  ipv4 = input()
  target = socket.gethostbyname(ipv4)
+ if (platform.system() == "Windows"):
+  os.system("ping -n "+count+ipv4)
+ if  (platform.system() == "Linux"):
+  os.system("ping -c "+count+ipv4)
+ else :
+  print("İnvalid İp")
+
 
  for port in range(1, 65535):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,7 +28,8 @@ def main():
 
   if result == 0:
    print(f"Port {port} is open ")
-  s.close()
+   s.close()
+
 
 
 if __name__ == "__main__":
