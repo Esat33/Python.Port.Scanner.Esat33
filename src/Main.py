0ab635cc199b41133844eb126a78094210869935
Esat33 <mehmetesatakdogan47@gmail.com>
2024-10-04 22:41:24 +0300

    port sacanner exe

--- src/Main.py
+++ src/Main.py
@@ -13,17 +13,16 @@ def main():
  print("Enter the IP address you want to scan (IPv4 only): ")
  ipv4 = input()
  target = socket.gethostbyname(ipv4)
- if (platform.system() == "Windows"):
+ if platform.system() == "Windows":
   os.system("ping -n "+count+ipv4)
- if  (platform.system() == "Linux"):
+ if  platform.system() == "Linux":
   os.system("ping -c "+count+ipv4)
  else :
   print("İnvalid İp")
 
-
  for port in range(1, 65535):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.settimeout(1)
+  s.settimeout(0.75)
   result = s.connect_ex((target, port))
 
   if result == 0:
